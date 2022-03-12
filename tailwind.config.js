8e5ee76f1f6ae354ac60c18bb395c1dfc355d61f
alifprihantoro <alifmuryp@gmail.com>
2022-03-12 13:22:02 +0700

    add color main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   content: ['./layouts/**/*.{html,js}'],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        main: {
+          1: '#035a0e',
+          2: '#038811',
+          3: '#35a041',
+          3: '#b3f166',
+        }
+      },
+    },
   plugins: [],
-};
-
+  },
+}
